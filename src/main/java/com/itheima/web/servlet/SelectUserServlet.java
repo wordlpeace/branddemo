4b99adfef538af339b232e5045abd76435fede4f
xienan <847095385@qq.com>
2022-04-04 15:13:43 +0800

    登出功能

--- src/main/java/com/itheima/web/servlet/SelectUserServlet.java
+++ src/main/java/com/itheima/web/servlet/SelectUserServlet.java
@@ -11,7 +11,7 @@ public class SelectUserServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
         //1. 接收用户名
-        String username = request.getParameter("username");
+        String username = request.getParameter("login");
 
         //2. 调用service查询User对象
 
